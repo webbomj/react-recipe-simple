29dd55731f4f68abe2a3b67620198d860183f5c5
Windows <nethack@mail.ru>
2021-12-14 19:07:12 +0400

    added ingredients block in posts

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Post.modules.css'
 
 const Post = ({data}) => {
   const {recipe} = data;
   let imgSRC = 'https://via.placeholder.com/300';
-
+  let arrIngredients = [];
+  const [portions, setPortions] = useState(1)
   if (recipe.images.REGULAR) {
     imgSRC = recipe.images.REGULAR.url
+  }
+
+  if (recipe.ingredients) {
+    recipe.ingredients.forEach(el => {
+      let ingred = {
+        food: el.food,
+        quantity: el.quantity,
+        measure: el.measure,
+        image: el.image,
+      }
+      arrIngredients.push(ingred);
+    })
+  }
+
+  const incPortion = () => {
+    setPortions(prev => prev + 1)
+  }
+
+  const decPortion = () => {
+    setPortions(prev => prev - 1)
   }
 
   return (
@@ -21,6 +42,38 @@ const Post = ({data}) => {
             })}
           </ul>
         </div>
+      </div>
+      <div className='ingredients'>
+          <div className='ingredients__header'>
+            <h3 className='ingredients__title'>To make {portions} servings you need:</h3>
+            <div className='ingredients__panel'>
+              <button className='ingredients-panel__inc' onClick={() => incPortion()}>+</button>
+              <button className='ingredients-panel__dec' onClick={() => decPortion()}>-</button>
+            </div>
+          </div>
+          <div className='ingredients__images'>{arrIngredients.map(el => {
+            return <img className='ingredients__img' src={el.image} key={el.image} alt={el.food}/>
+          })}</div>
+          <div className='ingredients__list'>
+          <table>
+            <tbody>
+              <tr>
+                <td>Necessary ingredients</td>
+                <td>Quantity</td>
+                <td></td>
+              </tr>
+                {arrIngredients.map(el => {
+                  return (
+                    <tr key={el.food}>
+                    <td>{el.food}</td>
+                    <td>{el.quantity}</td>
+                    <td>{el.measure}</td>
+                    </tr>
+                  ) 
+                })}
+            </tbody>
+          </table>
+          </div>
       </div>
     </div>
   );
